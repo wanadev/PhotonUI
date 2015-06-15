3378f0e9c7fc3a767a45e77987a6757d7e09ea4a
Fabien LOISON <flo@flogisoft.com>
2015-06-15 11:44:31 +0200

    new spec organisation

--- test/spec/visual/image.js
+++ test/spec/visual/image.js
@@ -1,36 +1,3 @@
-describe("photonui.Image (widget logic)", function() {
-
-    beforeEach(function() {
-        this.c = new DummyContainer();
-        this.w = new photonui.Image();
-    });
-
-    afterEach(function() {
-        this.c.destroy();
-        this.w.destroy();
-    });
-
-    it("can be unparented", function() {
-        this.c.child = this.w;
-        this.w.unparent();
-
-        expect(this.c.child).toBeNull();
-        expect(this.c.childName).toBeNull();
-        expect(this.c.containerNode.childNodes.length).toEqual(0);
-    });
-
-    it("is unparented when it is destroyed", function() {
-        this.c.child = this.w;
-        this.w.destroy();
-
-        expect(this.c.child).toBeNull();
-        expect(this.c.childName).toBeNull();
-        expect(this.c.containerNode.childNodes.length).toEqual(0);
-    });
-
-});
-
-
 describe("photonui.Image", function() {
 
     beforeAll(function () {
@@ -46,7 +13,6 @@ describe("photonui.Image", function() {
     });
 
     it("can change its image", function(done) {
-
         var that = this;
         this.img.__html.image.onload = function() {
             expect(parseFloat(that.img.__html.image.width)).toEqual(100);
@@ -60,7 +26,6 @@ describe("photonui.Image", function() {
     });
 
     it("can have a defined width", function(done) {
-        
         var that = this;
         this.img.__html.image.onload = function() {
             setTimeout(function () {
@@ -73,11 +38,9 @@ describe("photonui.Image", function() {
 
         this.img.width = 200;
         this.img.url = "./dummyimage.png";
-        
     });
 
     it("can have a defined height", function(done) {
-        
         var that = this;
         this.img.__html.image.onload = function() {
             setTimeout(function () {
@@ -90,11 +53,9 @@ describe("photonui.Image", function() {
 
         this.img.height = 200;
         this.img.url = "./dummyimage.png";
-        
     });
 
     it("can have both height and width defined", function(done) {
-        
         var that = this;
         this.img.__html.image.onload = function() {
             setTimeout(function () {
@@ -108,7 +69,6 @@ describe("photonui.Image", function() {
         this.img.width = 50;
         this.img.height = 200;
         this.img.url = "./dummyimage.png";
-        
     });
 
 });
