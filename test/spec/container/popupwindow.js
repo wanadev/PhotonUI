889d2272df26db4a28c83174625c783fd09bed1e
Fabien LOISON <flo@flogisoft.com>
2015-07-23 16:25:30 +0200

    PopupWindow test files

---
+++ test/spec/container/popupwindow.js
@@ -0,0 +1,20 @@
+describe("photonui.PopupWindow", function() {
+
+    beforeAll(function() {
+        // ...
+    });
+
+    beforeEach(function() {
+        // ...
+    });
+
+    afterEach(function() {
+        // ...
+    });
+
+    // it("<DESCRIPTION>", function() {
+    //     // EXPECTATIONS
+    // });
+
+});
+
