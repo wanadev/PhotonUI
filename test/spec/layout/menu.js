790e67a0325cc8040dbc6dc9f88e33b8bc211da0
Fabien LOISON <flo@flogisoft.com>
2015-07-23 16:34:00 +0200

    Menu test files

---
+++ test/spec/layout/menu.js
@@ -0,0 +1,20 @@
+describe("photonui.Menu", function() {
+
+    beforeAll(function() {
+        // ...
+    });
+
+    beforeEach(function() {
+        // ...
+    });
+
+    afterEach(function() {
+        // ...
+    });
+
+    // it("<DESCRIPTION>", function() {
+    //     // EXPECTATIONS
+    // });
+
+});
+
