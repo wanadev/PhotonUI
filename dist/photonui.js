367a8bbc92b5b91f80b4b5846e5ecccb2be5f3c0
Fabien LOISON <flo@flogisoft.com>
2015-09-22 14:04:13 +0200

    Fixes 'click' callback called twice on ToggleButton (closes #17)

--- dist/photonui.js
+++ dist/photonui.js
@@ -8729,7 +8729,7 @@ var ToggleButton = CheckBox.$extend({
 
     // Constructor
     __init__: function (params) {
-        this._registerWEvents(["click"]);
+        //this._registerWEvents(["click"]);
         this.$super(params);
         this.__buttonInit();
         this.removeClass("photonui-checkbox");
@@ -8741,7 +8741,7 @@ var ToggleButton = CheckBox.$extend({
     // photonui.Button constructor (without the call to $super)
     __buttonInit: function () {
         // Bind js events
-        this._bindEvent("click", this.__html.button, "click", this.__onButtonClicked.bind(this));
+        //this._bindEvent("click", this.__html.button, "click", this.__onButtonClicked.bind(this));
 
         // Update properties
         this._updateProperties(["text", "leftIconName", "rightIconName"]);
