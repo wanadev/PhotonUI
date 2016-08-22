wanadev/PhotonUI

commit 39f60ba34cb55e8762d89b0556dd51da8cccf8a4

MouseManager: fixes dragging event triggered twice (close #21)

src/nonvisual/mousemanager.js
@@ -631,6 +631,7 @@ var MouseManager = Base.$extend({
      * @param event
      */
     __onMouseDown: function (event) {
+        event.stopPropagation();
         this._stateMachine("mouse-down", event);
     },
 
@@ -640,6 +641,7 @@ var MouseManager = Base.$extend({
      * @param event
      */
     __onMouseUp: function (event) {
+        event.stopPropagation();
         this._stateMachine("mouse-up", event);
     },
 
@@ -658,6 +660,7 @@ var MouseManager = Base.$extend({
      * @param event
      */
     __onMouseMove: function (event) {
+        event.stopPropagation();
         this._stateMachine("mouse-move", event);
     },
 

commit ccf072479fbc86a8f5f75ff3239b5e92152d671d

Fixes issue with event propagation introduced by commit 39f60ba (closes #58, also a replacement fix for issue #21)

src/nonvisual/mousemanager.js
@@ -631,7 +631,6 @@ var MouseManager = Base.$extend({
      * @param event
      */
     __onMouseDown: function (event) {
-        event.stopPropagation();
         this._stateMachine("mouse-down", event);
     },
 
@@ -641,7 +640,6 @@ var MouseManager = Base.$extend({
      * @param event
      */
     __onMouseUp: function (event) {
-        event.stopPropagation();
         this._stateMachine("mouse-up", event);
     },
 
@@ -660,7 +658,6 @@ var MouseManager = Base.$extend({
      * @param event
      */
     __onMouseMove: function (event) {
-        event.stopPropagation();
         this._stateMachine("mouse-move", event);
     },
 
@@ -672,6 +669,9 @@ var MouseManager = Base.$extend({
      * @param event
      */
     __onDocumentMouseUp: function (event) {
+        if (event.target === this._element) {
+            return;
+        }
         if (this.action == "dragging" || this.action == "drag-start") {
             this._stateMachine("drag-end", event);
         }
@@ -685,6 +685,9 @@ var MouseManager = Base.$extend({
      * @param event
      */
     __onDocumentMouseMove: function (event) {
+        if (event.target === this._element) {
+            return;
+        }
         if (this.action == "dragging" || this.action == "drag-start") {
             this._stateMachine("dragging", event);
         }